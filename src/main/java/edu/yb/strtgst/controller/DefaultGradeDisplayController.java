7d7b0bb7ce7f3137a21acf1a5639c1a691cf8f3a
yasasbanukaofficial <yasasbanukaofficial@gmail.com>
2025-08-09 17:57:55 +0530

    chore(grade-fxml): removed unecessary files

--- src/main/java/edu/yb/strtgst/controller/DefaultGradeDisplayController.java
+++
@@ -1,9 +0,0 @@
-package edu.yb.strtgst.controller;
-
-import javafx.scene.input.MouseEvent;
-
-public class DefaultGradeDisplayController {
-    public void addNewGrade(MouseEvent mouseEvent) {
-        
-    }
-}
